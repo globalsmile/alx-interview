ec32f03b3fb0a59a0b4bf97cd88854942d57520f
globalsmile <mohammedbadmus823@gmail.com>
2024-01-09 21:19:55 +0100

    add: lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#/usr/bin/python3
+""" Lockboxes """
+def canUnlockAll(boxes):
+    """ Lockboxes """
+    if len(boxes) == 0:
+        return False
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes):
+                keys.append(box)
+    if len(keys) == len(boxes):
+        return True
+    return False
