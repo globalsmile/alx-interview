7e06a50c3acc76d8cec9b4a947de4441fc722d1c
globalsmile <mohammedbadmus823@gmail.com>
2024-01-26 05:34:18 +0100

    modified: 0-stats.py file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -13,25 +13,26 @@ def print_stats(file_size, status_codes):
 
 
 if __name__ == "__main__":
+    """Main method"""
+    file_size = 0
     status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                     "403": 0, "404": 0, "405": 0, "500": 0}
-    file_size = 0
-    count = 0
+    counter = 0
+
     try:
         for line in sys.stdin:
-            count += 1
-            data = line.split()
+            counter += 1
+            parsed = line.split()
             try:
-                file_size += int(data[-1])
+                file_size += int(parsed[-1])
             except ValueError:
                 pass
             try:
-                status_codes[data[-2]] += 1
+                status_codes[parsed[-2]] += 1
             except KeyError:
                 pass
-            if count == 10:
+            if counter % 10 == 0:
                 print_stats(file_size, status_codes)
-                count = 0
         print_stats(file_size, status_codes)
     except KeyboardInterrupt:
         print_stats(file_size, status_codes)
