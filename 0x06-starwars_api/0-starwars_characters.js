730a5f11597cb8261c39a9dc9a744c2b768178a2
globalsmile <mohammedbadmus823@gmail.com>
2024-02-16 10:20:32 +0100

    prefix unused variables

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,14 +4,14 @@ const request = require('request')
 const movieId = process.argv[2]
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`
 
-request(url, async function (error, response, body) {
+request(url, async function (error, _response, body) {
   if (error) {
     console.log(error)
   } else {
     const characters = JSON.parse(body).characters
     for (const character of characters) {
       const name = await new Promise((resolve, reject) => {
-        request(character, function (error, response, body) {
+        request(character, function (error, _response, body) {
           if (error) {
             reject(error)
           } else {
