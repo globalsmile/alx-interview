055ce8e4fe834899cb19345625554b748b39dc64
globalsmile <mohammedbadmus823@gmail.com>
2024-02-16 08:58:29 +0000

    fixed lint style

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,25 @@
 #!/usr/bin/node
 
-const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+const request = require('request')
+const movieId = process.argv[2]
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`
 
 request(url, async function (error, response, body) {
-    if (error) {
-        console.log(error);
-    } else {
-        const characters = JSON.parse(body).characters;
-        for (const character of characters) {
-        const name = await new Promise((resolve, reject) => {
-            request(character, function (error, response, body) {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(JSON.parse(body).name);
-            }
-            });
-        });
-        console.log(name);
-        }
+  if (error) {
+    console.log(error)
+  } else {
+    const characters = JSON.parse(body).characters
+    for (const character of characters) {
+      const name = await new Promise((resolve, reject) => {
+        request(character, function (error, response, body) {
+          if (error) {
+            reject(error)
+          } else {
+            resolve(JSON.parse(body).name)
+          }
+        })
+      })
+      console.log(name)
     }
-});
+  }
+})
