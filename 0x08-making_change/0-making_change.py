79b88b3c0bd157b666a31cfc6c4653f7be84fe97
globalsmile <mohammedbadmus823@gmail.com>
2024-03-01 08:32:51 +0100

    add: 0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+""" making change problem """
+
+
+def makeChange(coins, total):
+    """ make change function """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        if total == 0:
+            return count
+        if total < coin:
+            continue
+        count += total // coin
+        total = total % coin
+    if total != 0:
+        return -1
+    return count
